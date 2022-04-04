7cd911af4f17f10aa4749f9aa99081b5863df2ee
gyfoh <lamark.barros@gmail.com>
2022-04-04 18:25:52 -0300

    implementa nova app.js

--- task-org/src/App.js
+++ task-org/src/App.js
@@ -1,8 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './components/pages/Home'
+import Tasks from './components/pages/Tasks'
+import Contact from './components/pages/Contact'
+import EditTask from './components/pages/EditTask'
+import NewTask from './components/pages/NewTask'
+
+import Container from './components/layout/Container'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+
 function App() {
   return (
-    <div className="App">
-      <p>Task Organizer</p>
-    </div>
+    <Router>
+      <Navbar/>
+      <Container customClass="min-height">
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/tasks" element={<Tasks/>}></Route>
+          <Route path="/contact" element={<Contact/>}></Route>
+          <Route path="/edittask" element={<EditTask/>}></Route>
+          <Route path="/newtask" element={<NewTask/>}></Route>
+        </Routes>
+      </Container>
+      <Footer/>
+    </Router>
   );
 }
 
