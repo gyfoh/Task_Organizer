0d09d588fbaed0eea15e9a702c4ed1e717e9b876
gyfoh <lamark.barros@gmail.com>
2022-04-04 18:26:33 -0300

    implementa a footer e seu css

---
+++ task-org/src/components/layout/Footer.js
@@ -0,0 +1,17 @@
+import {FaGithub, FaLinkedin} from 'react-icons/fa'
+
+import styles from './Footer.module.css'
+
+function Footer(){
+    return(
+        <footer className={styles.footer}>
+            <ul className={styles.social_list}>
+                <li><FaGithub/></li>
+                <li><FaLinkedin/></li>
+            </ul>
+            <p className={styles.copy_right}><span>TaskOrganizer</span> &copy;2021</p>
+        </footer>
+    )
+}
+
+export default Footer
